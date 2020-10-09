fb9d548cd81f4cda44b93ffe7bd2bca4c527d684
Justin Yu <yujustin@umich.edu>
2020-10-08 22:55:43 -0400

    sample datasets added

--- CSV/matplotlib_from_csv.py
+++ CSV/matplotlib_from_csv.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-data = np.genfromtxt("sample.csv" , delimiter=" ", names=["x", "y"])
+filename = 'sample2020-10-8_22_53_45'
+data = np.genfromtxt(filename+".csv" , delimiter=" ", names=["x", "y"])
 
 plt.plot(data['x'],data['y'])
-plt.savefig("sample"now".png")
+plt.savefig(filename+".png")
--- CSV/randomCSVgen.py
+++ CSV/randomCSVgen.py
@@ -1,15 +1,16 @@
 import csv
 import os
+import subprocess
 import datetime
 import numpy as np
 
+#touches new file in playground directory
 now = datetime.datetime.now()
 filename = "sample%d" %now.year+"-%d" %now.month+"-%d" %now.day+"_%d" %now.hour+"_%d" %now.minute+"_%d" %now.second+ ".csv"
 filename
-os.system('cd /home/yujustin/Desktop/playground/CSV/rngCSV/ \ntouch'+filename)
-#os.system('touch '+ filename)
+subprocess.call('touch '+filename, shell=True)
 
-#generates csv file with 1000 rows and 2 columns where first column is index
+#generates csv file with 50 rows and 2 columns where first column is index
 with open(filename, 'w') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
