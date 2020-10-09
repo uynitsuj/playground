e8740f4a79e79e82628bb2332cb20441be58d8c1
Justin Yu <yujustin@umich.edu>
2020-10-08 22:27:02 -0400

    sequential file generator

--- CSV/matplotlib_from_csv.py
+++ CSV/matplotlib_from_csv.py
@@ -2,8 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
 data = np.genfromtxt("sample.csv" , delimiter=" ", names=["x", "y"])
 
 plt.plot(data['x'],data['y'])
-plt.savefig("sample.png")
+plt.savefig("sample"now".png")
--- CSV/randomCSVgen.py
+++ CSV/randomCSVgen.py
@@ -1,8 +1,16 @@
 import csv
+import os
+import datetime
 import numpy as np
 
+now = datetime.datetime.now()
+filename = "sample%d" %now.year+"-%d" %now.month+"-%d" %now.day+"_%d" %now.hour+"_%d" %now.minute+"_%d" %now.second+ ".csv"
+filename
+os.system('cd /home/yujustin/Desktop/playground/CSV/rngCSV/ \ntouch'+filename)
+#os.system('touch '+ filename)
+
 #generates csv file with 1000 rows and 2 columns where first column is index
-with open('sample.csv', 'w') as csvfile:
+with open(filename, 'w') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     for x in range(0,50):
